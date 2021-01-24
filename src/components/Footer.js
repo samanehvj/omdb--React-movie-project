1a1a62d0ff36cb00a18014990d28f9eb148d18af
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-01-24 13:56:40 -0800

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = (props) => {
+  return (
+    <footer className="App-footer">
+      <h2>{props.text}</h2>
+    </footer>
+  );
+};
+
+export default Footer;
